d67466a9524cce77aa36dabc881c887c7dc69339
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2016-07-07 13:03:30 +0530

    Source Code moved into correct directories based on Groups in Project Navigator

---
+++ iOSFlickrSample/BaseNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseNavigationController.swift
+//  iOSFlickrSample
+//
+//  Created by Sai Teja Jammalamadaka on 7/7/16.
+//  Copyright © 2016 tsjamm. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationController: UINavigationController {
+    
+    
+    
+}
