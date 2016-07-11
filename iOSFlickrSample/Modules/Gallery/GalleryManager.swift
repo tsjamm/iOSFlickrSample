c7a9ee31563f50ff808bd7ab67d3c5ea833dfb6d
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2016-07-11 16:34:01 +0530

    removed some unnecessary code in Gallery Manager

--- iOSFlickrSample/Modules/Gallery/GalleryManager.swift
+++ iOSFlickrSample/Modules/Gallery/GalleryManager.swift
@@ -10,48 +10,22 @@ import UIKit
 
 class GalleryManager {
     
-    private static var flickrSearchStack = [String]()
-    
-    static func clearSearches() {
-        flickrSearchStack.removeAll()
-    }
-    
-    static func getNumberOfSearches() -> Int {
-        return self.flickrSearchStack.count
-    }
+    private static var currentSearchTerm: String = ""
     
     static func getNumberOfPhotos(forSearchIndex:Int) -> Int {
-        let searchTerm = self.flickrSearchStack[forSearchIndex]
-        return FlickrDataManager.getCachedFlickrResponse(searchTerm)?.photo.count ?? 0
-    }
-    
-    static func getFlickrResponseForIndexPath(indexPath:NSIndexPath) -> FlickrResponse {
-        let section = indexPath.section
-        let searchTerm = self.flickrSearchStack[section]
-        return FlickrDataManager.getCachedFlickrResponse(searchTerm) ?? FlickrResponse(dataMap: [:])
+        return FlickrDataManager.getCachedFlickrResponse(currentSearchTerm)?.photo.count ?? 0
     }
     
     static func getFlickrPhotoForIndexPath(indexPath:NSIndexPath) -> FlickrPhoto? {
-        let section = indexPath.section
-        let searchTerm = self.flickrSearchStack[section]
         let row = indexPath.row
-        return FlickrDataManager.getCachedFlickrResponse(searchTerm)?.photo[row]
+        return FlickrDataManager.getCachedFlickrResponse(currentSearchTerm)?.photo[row]
     }
     
     static func fetchFlickrData(searchTerm:String, callback:((FlickrResponse)->())) {
         
-        if let toRemoveIndex = self.flickrSearchStack.indexOf(searchTerm) {
-            self.flickrSearchStack.removeAtIndex(toRemoveIndex)
-        }
-        
-        self.flickrSearchStack.insert(searchTerm, atIndex: 0)
+        self.currentSearchTerm = searchTerm
         FlickrDataManager.fetchFlickerData(searchTerm) { (fResponse) in
             callback(fResponse)
         }
-    }
-    
-    static func clearFlickrData(callback:(()->())) {
-        self.flickrSearchStack.removeAll()
-        callback()
     }
 }
